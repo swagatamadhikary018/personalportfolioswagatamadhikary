3f2a7dead4d335d8584d1f435e1c8c92e13dbe67
Swagatam Adhikary <133666171+swagatamadhikary018@users.noreply.github.com>
2024-09-19 12:04:10 +0530

    Add files via upload

---
+++ autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>  Hii Everyone! Myself Swagatam Adhikary, Enthusiastic and motivated BCA student with a current CGPA of 8.34. a passionate BCA Graduate From Techno India Hooghly. I'm absolutely grateful to introduce myself and share my thoughts here. I was born in Singur, Hooghly, West Bengal,India. I started my schooling from Singur Mahamaya High School and Passed Madhyamik Examination in 2019 with 65% marks. Then I'm started my Higher Secondary Education from Singur Mahamaya High School and passed my HS examination in 2021 with 75% marks. In addition to practical online courses experience, I hold certifications that validate my skills, including Google's Data Analytics Certificate. I've also earned a certificate Career Essentials in Software Development by Microsoft and LinkedIn. I am a passionate Web Developer, Programmer, FrontEnd Developer and FullStack Developer. Currently, I am passionate my skills in full-stack web development. I am a passionate Web Developer , Programmer. I've Honured My Skills In Web Development and Programming. Beyond programming lanuguages, a strong knowledge in Microsoft Word, Excel and PowerPoint. I am eager to collaborate on innovative projects on Web Development. Here are the skilled in multiple programming languages: Web Development, DBMS, Python, PHP, Data Structure and MySQL and so on. I am a highly motivated and results-oriented full-stack web developer with a passion for building innovative and user-friendly applications.Effective communicator in English, Bengali, and Hindi. Dedicated to continuous learning and self-improvement. I have also experienced in Programming Language like C,Java,Python. I have successfully completed several projects using HTML, CSS, JavaScript, PHP, MySQL, Bootstrap, Laravel these technologies. That's all about myself, Thank You. </span>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+
+    });
+},   100);
